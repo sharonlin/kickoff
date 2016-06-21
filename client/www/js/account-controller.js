865b3478e1514e725d8e8f48f075b50edf420976
Sharon Lin <sharon.lin.1@gmail.com>
2016-06-22 01:57:05 +0300

    Change profile layout,add profile image

--- client/www/js/account-controller.js
+++ client/www/js/account-controller.js
@@ -1,11 +1,23 @@
 app.controller('AccountController', function ($scope, $ionicModal,UsersService,$cordovaCamera, $cordovaFileTransfer) { //store the entities name in a variable var taskData = 'task';
-
+$scope.profileImageUrl = '/img/profile.jpg';
 $ionicModal.fromTemplateUrl('create-profile-modal', {
     scope: $scope,
     animation: 'slide-in-up'
 }).then(function (modal) {
     $scope.profileDialogModal = modal;
 });
+
+$scope.uploadProfileImage = function(){
+	
+	$scope.profileImageUrl = '/img/ionic.png';
+};
+$scope.calculateAge = function(){
+	
+	var birthdate = new Date($scope.user.birthday);
+	var cur = new Date();
+	var diff = cur-birthdate; // This is the difference in milliseconds
+	return Math.floor(diff/31536000000); // Divide by 1000*60*60*24*365
+};
 
 $scope.openProfileDialogModal = function(){
 	$scope.profileDialogModal.show();
--- client/www/js/services/users-service.js
+++ client/www/js/services/users-service.js
@@ -16,11 +16,11 @@ function(localStorageService) {
 		// if (localStorageService.get('users')) {
 			// return localStorageService.get('users');
 		// }
-		return [{"strongLeg":"Right","description":"dribbles","country":"Germany","birthday":"2016-06-07T21:00:00.000Z","tafkid":"Striker","name":"Yorgen","family":"Yorg"},
-		{"strongLeg":"Right","description":"Some desc","country":"England","birthday":"2016-06-07T21:00:00.000Z","tafkid":"game maker","name":"Jack","family":"Sparrow"},
-		{"name":"Yossi","family":"Swissa","birthday":"2016-06-19T21:00:00.000Z","country":"Israel","tafkid":"Defender","strongLeg":"Right","description":"ststs"},
-		{"name":"testestststs","family":"te","birthday":"2016-06-19T21:00:00.000Z","country":"ststs","tafkid":"ete","strongLeg":"Both","description":"ststs"},
-		{"name":"rrr","family":"rr","birthday":"2016-05-29T21:00:00.000Z","tafkid":"rw","strongLeg":"Right","description":"rw"}];
+		return [{"dominantLeg":"Right","description":"dribbles","country":"Germany","birthday":"2016-06-07T21:00:00.000Z","position":"Striker","name":"Yorgen","family":"Yorg"},
+		{"dominantLeg":"Right","description":"Some desc","country":"England","birthday":"2016-06-07T21:00:00.000Z","position":"game maker","name":"Jack","family":"Sparrow"},
+		{"name":"Yossi","family":"Swissa","birthday":"2016-06-19T21:00:00.000Z","country":"Israel","tafkid":"Defender","dominantLeg":"Right","description":"ststs"},
+		{"name":"testestststs","family":"te","birthday":"2016-06-19T21:00:00.000Z","country":"ststs","position":"ete","dominantLeg":"Both","description":"ststs"},
+		{"name":"rrr","family":"rr","birthday":"2016-05-29T21:00:00.000Z","position":"rw","dominantLeg":"Right","description":"rw"}];
 	}
 
 	return {
